f67871f13d8a750c9b632fa29b9857e833a37376
memory-killer <low.memory.killer@gmail.com>
2016-06-28 11:57:44 +0900

    Update build.js (#6)

--- scripts/build.js
+++ scripts/build.js
@@ -8,15 +8,18 @@ var building_list = ["academy", "timber_camp", "clay_pit", "iron_mine", "headqua
                   "warehouse", "farm", "tavern", "hospital", "preceptory",
                   "church", "chapel", "rally_point", "statue", "market", "wall"];
 
-var MAX_HQ = 25;
+var HQ_ACDM = 20;
+var HQ_PREC = 25;
+var MAX_ACDM = 1;
+var MAX_PRECEP = 10;
 var MAX_FARM = 30;
 var MAX_WAREHOUSE = 30;
+var MAX_WALL = 20;
 var MAX_RALLY = 5;
 var MAX_STATUE = 5;
-var MAX_WALL = 20;
-var HC_BARRACKS = 21;
+var BARRACKS_HC = 21;
 var MAX_MARKET = 25;
-var MAX_PRECEP = 10;
+
 
 function makeVillage_list(){
     socketService.emit(routeProvider.CHAR_GET_INFO, {}, function(data){
@@ -38,7 +41,11 @@ function buildBuildings(){
 
       for(j=0; j < village_list.length; j++){
 
-        if(data.resources.food < 500 && data.buildings["farm"].level < MAX_FARM){
+        if(data.buildings["wall"].level < MAX_WALL){
+          socketService.emit(routeProvider.VILLAGE_UPGRADE_BUILDING, {
+            village_id: village_list[j], building: "wall", premium: 0, location: 0}, function(data){});
+        }
+        if(data.resources.food < 1000 && data.buildings["farm"].level < MAX_FARM){
           console.log("We need farm!");
           socketService.emit(routeProvider.VILLAGE_UPGRADE_BUILDING, {
             village_id: village_list[j], building: "farm", premium: 0, location: 0}, function(data){});       
@@ -47,27 +54,27 @@ function buildBuildings(){
           socketService.emit(routeProvider.VILLAGE_UPGRADE_BUILDING, {
             village_id: village_list[j], building: "warehouse", premium: 0, location: 0}, function(data){});
         }
-        if(data.buildings["barracks"].level < HC_BARRACKS){
+        if(data.buildings["barracks"].level < BARRACKS_HC){
           socketService.emit(routeProvider.VILLAGE_UPGRADE_BUILDING, {
             village_id: village_list[j], building: "barracks", premium: 0, location: 0}, function(data){});
-        }
-        if(data.buildings["wall"].level < MAX_WALL){
-          socketService.emit(routeProvider.VILLAGE_UPGRADE_BUILDING, {
-            village_id: village_list[j], building: "wall", premium: 0, location: 0}, function(data){});
         }
         if(data.buildings["rally_point"].level < MAX_RALLY){
           socketService.emit(routeProvider.VILLAGE_UPGRADE_BUILDING, {
             village_id: village_list[j], building: "rally_point", premium: 0, location: 0}, function(data){});
         }
-        if(data.buildings["headquarter"].level < MAX_HQ){
-          socketService.emit(routeProvider.VILLAGE_UPGRADE_BUILDING, {
-            village_id: village_list[j], building: "headquarter", premium: 0, location: 0}, function(data){});
-        }
         if(data.buildings["statue"].level < MAX_STATUE){
           socketService.emit(routeProvider.VILLAGE_UPGRADE_BUILDING, {
             village_id: village_list[j], building: "statue", premium: 0, location: 0}, function(data){});
         }
-        if(data.buildings["headquarter"].level >= MAX_HQ && data.buildings["preceptory"].level < MAX_PRECEP){
+        if(data.buildings["headquarter"].level < HQ_PREC){
+          socketService.emit(routeProvider.VILLAGE_UPGRADE_BUILDING, {
+            village_id: village_list[j], building: "headquarter", premium: 0, location: 0}, function(data){});
+        }
+        if(data.buildings["headquarter"].level >= HQ_ACDM && data.buildings["academy"].level < MAX_ACDM){
+          socketService.emit(routeProvider.VILLAGE_UPGRADE_BUILDING, {
+            village_id: village_list[j], building: "academy", premium: 0, location: 0}, function(data){});
+        }
+        if(data.buildings["headquarter"].level >= HQ_PREC && data.buildings["preceptory"].level < MAX_PRECEP){
           socketService.emit(routeProvider.VILLAGE_UPGRADE_BUILDING, {
             village_id: village_list[j], building: "preceptory", premium: 0, location: 0}, function(data){});
         }
@@ -84,22 +91,3 @@ function buildBuildings(){
 
 console.log("It's BUILDING time! ^^");
 makeVillage_list();
-
-
-/*
-// This code block works OK, but I don't need it now.
-function getBuilding_Levels(){
-  for(i=0; i < village_list.length; i++){
-
-    socketService.emit(routeProvider.VILLAGE_GET_VILLAGE ,{village_id: village_list[i]}, function(data){
-        for(j = 0; j < building_list.length; j++){
-          console.log(building_list[j], "is level: ", data.buildings[building_list[j]].level)
-          console.log(data.buildings[building_list[j]].level)
-        }
-      });
-
-  }
-  setTimeout(buildBuildings, 5000);
-}
-*/
-
