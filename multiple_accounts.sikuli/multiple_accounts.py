42a0d69f778681132ce21b956918d9a1eb817e90
Jinwoo Ahn <jinwoo.ahn@lge.com>
2016-07-23 01:09:14 +0900

    Porting sikuli environment from Chrome to Firefox
    We used chromium-browser before. Chrome is fast, but unstable when running TW2 scripts.
    I recognized that the script does not work sometimes in Chrome.
    So I modified the sikuli script for Firefox.
    I assume the user already stored his id/passwd in Firefox.
    So we don't need to write password in the script from now.

--- multiple_accounts.sikuli/multiple_accounts.py
+++ multiple_accounts.sikuli/multiple_accounts.py
@@ -1,47 +1,57 @@
+# We assume that
+# 1. We use Firefox.
+# 2. The password has already stored in Firefox.
+
 # 멀티 계정 관리
 login_list = [
-        (아이디,비밀번호),
-        (아이디,비밀번호),
-        (아이디,비밀번호),
+        "아이디",
+        "아이디",
+        "아이디",
         ]
+
+# ex. Total villages = 4 * 10 = 40
+vill_iter = 4 # You should edit this
 
 
 def open_browser():
     sleep(5)
-    openApp("chromium-browser")
-    wait("1463996150597.png", 30)
-    click("1463996022613.png")
-    type("www.tribalwars2.com"+Key.ENTER)
-    wait("1463997177505.png", 30)
+    openApp("firefox") # We use Firefox, because the script can work stably. More stable than Chrome.
+    sleep(10)
+    sleep(5)
+    wait("1469194780023.png",30)
+    click("1469194780023.png")
+    sleep(5)
+    type("www.")
+    sleep(1)
+    type("tribal")
+    sleep(1)
+    type("wars2.")
+    sleep(1)
+    type("com")
+    sleep(1)
+    type(Key.ENTER)
+    wait("1469195055526.png",30)
 
 def close_browser():
     type(Key.F4, KeyModifier.ALT)
     sleep(5)
-    
+
 def pop_up_handling():
-    if exists("1464578586745.png"):
-        click("1464578586745.png")
-        sleep(5)
-    
-    if exists("1464578628552.png"):
-        click("1464578720862.png")
-        type("0000"+Key.ENTER)
+    # Claim pop up
+    if exists("1469199662214.png",10):
+        click("1469199662214.png")
         sleep(5)
 
-def pop_up_handling2():
-    # Claim pop up    
-    if exists("1464079346152.png", 5): 
-        click("1464079346152.png")
-        sleep(5)
-            
 def login(account):
-    click("1463996706288.png")
-    type(account[0]+Key.TAB+account[1]+Key.ENTER)
-    
-    pop_up_handling()
+    click("1469195035292.png")
+    type(account) # id
+    sleep(1)
+    type(Key.TAB) # We assume that the password has already stored in Firefox.
+    sleep(1)
+    type(Key.ENTER)
     sleep(5)
-    if exists("1464839920474.png", 120):
-        click("1464839920474.png")
+    wait("1469196068206.png",30)
+    click("1469196068206.png")
 
 def wait_for_loading():
     sleep(5)
@@ -54,18 +64,22 @@ def open_console():
 def building(account, loop):
     click("1463996398215.png")
     sleep(5)
-    
+
     # 마을이 너무 많은 계정일 경우 마을 10개 단위로 컨트롤
-    if account[0] == "memory.killer":
-        if loop % 2 == 1:
+    if account == "memory.killer":
+        if loop % vill_iter == 1:
             type("cat ~/etc/tw/scripts/build1.js | xclip -i -selection clipboard"+Key.ENTER)
-        elif loop % 2 == 0:
+        elif loop % vill_iter == 2:
             type("cat ~/etc/tw/scripts/build2.js | xclip -i -selection clipboard"+Key.ENTER)
+        elif loop % vill_iter == 3:
+            type("cat ~/etc/tw/scripts/build3.js | xclip -i -selection clipboard"+Key.ENTER)            
+        else:
+            type("cat ~/etc/tw/scripts/build4.js | xclip -i -selection clipboard"+Key.ENTER)
     else:
         type("cat ~/etc/tw/scripts/build.js | xclip -i -selection clipboard"+Key.ENTER)
 
     sleep(5)
-    click("1463996268415.png")
+    click("1469194883617.png")
     sleep(5)
     cb = Env.getClipboard()
     paste(cb)
@@ -77,16 +91,20 @@ def farming(account, loop):
     sleep(5)
     
     # 마을이 너무 많은 계정일 경우 마을 10개 단위로 컨트롤
-    if account[0] == "memory.killer":
-        if loop % 2 == 1:
+    if account == "memory.killer":
+        if loop % vill_iter == 1:
             type("cat ~/etc/tw/scripts/farm1.js | xclip -i -selection clipboard"+Key.ENTER)
-        elif loop % 2 == 0:
+        elif loop % vill_iter == 2:
             type("cat ~/etc/tw/scripts/farm2.js | xclip -i -selection clipboard"+Key.ENTER)
+        elif loop % vill_iter == 3:
+            type("cat ~/etc/tw/scripts/farm3.js | xclip -i -selection clipboard"+Key.ENTER)            
+        else:
+            type("cat ~/etc/tw/scripts/farm4.js | xclip -i -selection clipboard"+Key.ENTER)
     else:
         type("cat ~/etc/tw/scripts/farm.js | xclip -i -selection clipboard"+Key.ENTER)
 
     sleep(5)
-    click("1463996268415.png")
+    click("1469194883617.png")
     sleep(5)
     cb = Env.getClipboard()
     paste(cb)
@@ -97,21 +115,21 @@ def farming(account, loop):
 loop=1
 while loop:
     for account in login_list:
-        
+
         # Building Turn
         open_browser()
         login(account)
         wait_for_loading()
-        pop_up_handling2()    
+        pop_up_handling()    
         open_console()
         building(account, loop)
         close_browser()
-    
+
         # Farming Turn
         open_browser()
         login(account)
         wait_for_loading()
-        pop_up_handling2()    
+        pop_up_handling()
         open_console()
         farming(account, loop)
         close_browser()
@@ -120,5 +138,6 @@ while loop:
     #################################################################
     # wait for next iteration...
     loop += 1
+    
     sleep(90*60) # 90분 마다 스크립트 실행
     # end of "while True:"
